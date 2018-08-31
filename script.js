2df8fca08d0c6f32c73fa0208805e4a0ca1d4642
dkphl <dkae0809@gmail.com>
2018-08-31 00:07:25 -0400

    more styling, working js converter function

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const fahrenheitInput = document.querySelector("#input-fahr");
-const celciusInput = document.querySelector("#input-celc");
+const fahrenheitInput = document.querySelector('#input-fahr');
+const celciusInput = document.querySelector('#input-celc');
+const invalidChars = ['e', '+'];
+const preventLetterE = function (e) {
+  if (invalidChars.includes(e.key)) {
+    e.preventDefault();
+  }
+}
+
+fahrenheitInput.addEventListener('keydown', preventLetterE);
+celciusInput.addEventListener('keydown', preventLetterE);
+
+fahrenheitInput.addEventListener('input', function() {
+  celciusInput.value = convert.fahrenheitToC(fahrenheitInput.value);
+});
+
+celciusInput.addEventListener('input', function() {
+  fahrenheitInput.value = convert.celciusToF(celciusInput.value);
+});
 
 const convert = {
   celciusToF: function (celc) {
     let fahrenheit =  celc * 1.8 + 32;
     return fahrenheit.toFixed(1);
-
   },
   fahrenheitToC: function (fahr) {
     let celcius =  (fahr - 32) / 1.8;
@@ -13,8 +29,3 @@ const convert = {
   }
 }
 
-
-
-console.log(fahrenheitInput);
-console.log(convert.celciusToF(32));
-console.log(convert.fahrenheitToC(98));
